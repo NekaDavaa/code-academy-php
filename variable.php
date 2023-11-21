dbc9afe9e09bccc8f9aec241760b891341150bfe
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-11-21 21:41:45 +0200

    Practise different variable types

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+$string = 'asd';
+var_dump($string);
+$integer = 123;
+var_dump($integer);
+$float = 12.123;
+var_dump($float);
+$boolean = true;
+var_dump($boolean);
+$array = ['asd'];
+var_dump($array);
