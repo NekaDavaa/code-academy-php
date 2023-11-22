bce49db623ca8487dc73a99ce43b895fa662d89d
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-11-22 15:04:02 +0200

    practise strlen

--- variable.php
+++ variable.php
@@ -1,25 +1,13 @@
+<form method="POST">
+<input type="number" name="user_input">
+<input type="submit">
+</form>
+
 <?php
-$month = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
 
-$counter = 0;
-$skipCounter = 0;
+if (isset($_POST['user_input'])) {
+ $user_input = $_POST['user_input'];
 
-foreach ($month as $day) {
-    if ($skipCounter > 0) {
-        
-        $skipCounter--;
-       
-        continue;
-    }
+ echo $result = strlen($user_input);
 
-    echo $day . "<br>";
-
-    $counter++;
-    if ($counter % 5 == 0) {
-        $skipCounter = 2;
-    }
 }
-
-
-
-
