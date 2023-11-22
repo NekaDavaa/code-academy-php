f49362666023903543d89e67781c63dd0ffd1156
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-11-22 09:47:19 +0200

    practise rand array

--- variable.php
+++ variable.php
@@ -1,5 +1,15 @@
 <?php
 
-$input = 'nekadava';
+$week = ['Ponedelnik', 'Vtornik', 'Srqda', 'Chetvurtuk', 'Petuk', 'Subota', 'Nedelq'];
 
-echo str_pad($input, 60, "-", STR_PAD_BOTH);
+
+$inputKey = array_rand($week);
+$input = $week[$inputKey];
+echo $input . "<br>";
+
+if ($input === $week['0'] ||$input === $week['1'] || $input === $week['2'] ||$input === $week['3'] ||$input === $week['4']) {
+	echo "Happy work week";
+}
+else {
+	echo "Happy weekend";
+}
