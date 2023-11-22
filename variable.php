beab04ac406d6114e1d63236c9b6bb0fafc03eb3
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-11-22 10:32:33 +0200

    more practise while

--- variable.php
+++ variable.php
@@ -1,26 +1,17 @@
-<form method="POST">
-  <select name="title">
-    <option value="g-n">г-н</option>
-    <option value="g-ca">г-ца</option>
-    <option value="d-r">д-р</option>
-  </select>
-  <input type="submit" name="asd">
-</form>
-
-
 <?php
-if (isset($_POST['title'])) {
-	 $selected = $_POST['title'];
-	switch ($selected) {
-		case 'g-n':
-			echo "Gospodin";
-			break;
-		case 'g-ca':
-			echo "Gospojica";
-			break;
-		case 'd-r':
-			echo "Doktor";
-			break;
-	}
+
+
+$new_array = [];
+
+$i=0;
+
+while ($i <= 30) {
+	$i++;
+	$new_array[] = $i;
 }
 
+echo "<pre>";
+print_r($new_array);
+echo "</pre>";
+
+?>
