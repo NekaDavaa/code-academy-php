5e06e1c1edbda5d5f6610f4f737567ea7123378c
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-11-23 11:12:17 +0200

    practise arrays

--- variable.php
+++ variable.php
@@ -1,13 +1,26 @@
 <?php
-$arr = [5, 10, 15, 20, 50, 'asd', 50123];
-
-
-function calcArr($arr) {
-   $totalsum = array_sum($arr);
-   echo $totalsum;
+$book = [['PHP 8', 'D. K. Academy', '04-01-2022 г.', '490', '25BGN'], ['asd']];
+function showBook($book) {
+$table = "<table>";
+$table .= "<tr>";
+$table .= "<th>Заглавие</th>";
+$table .= "<th>Автор</th>";
+$table .= "<th>Година</th>";
+$table .= "<th>Страници</th>";
+$table .= "<th>Цена</th>";
+$table .= "</tr>";
+foreach ($book as $row) { 
+   $table .= "<tr>";
+    foreach ($row as $single) {
+        $table .= "<td>$single</td>";
+    }
+    $table .= "</tr>";
 }
+$table .= "</table>";
 
-calcArr($arr);
+return $table;
+}
+echo showBook($book);
 
 
 
