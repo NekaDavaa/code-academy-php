9ddffb31191033511b193f8146236abd802778d8
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-11-29 21:33:55 +0200

    asd

--- class_cap/api.php
+++ class_cap/api.php
@@ -1,1 +1,15 @@
 <?php
+interface Template
+{
+    public function setVariable($name, $var);
+    public function getHtml($template);
+}
+
+$greeting = new class implements Template {
+    public function setVariable($name, $var) {
+        // Method implementation
+    }
+    public function getHtml($template) {
+        // Method implementation
+    }
+};
