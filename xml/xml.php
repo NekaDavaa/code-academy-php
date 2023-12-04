de053655c6648bd1efcdd3898778e206b3d28fc3
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-12-04 20:26:17 +0200

    Update xml.php

--- xml/xml.php
+++ xml/xml.php
@@ -7,5 +7,13 @@ foreach ($xml->record as $product) {
     $products[] = $product;
 }
 
+function sortByName($a, $b) {
+    return strcmp($a->product_name, $b->product_name);
+}
 
-print_r ($products);
+usort($products, 'sortByName');
+
+foreach ($products as $product) {
+    echo "<img src='$product->brand'/>";
+    echo $product->product_name . "<br>"; 
+}
